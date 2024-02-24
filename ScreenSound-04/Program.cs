541efb9d11d9d3c5c582ff95e7aa5385c0d279af
ebagabe <gabriel.ebagabe@gmail.com>
2024-02-24 18:48:26 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound-04/Program.cs
@@ -0,0 +1,5 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    string reposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(reposta);
+}
