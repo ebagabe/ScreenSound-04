f59987c80ca89a081ad7d24df68bb8cd53db7ce2
ebagabe <gabriel.ebagabe@gmail.com>
2024-02-24 19:02:20 -0300

    Initial Commit

--- ScreenSound-04/Program.cs
+++ ScreenSound-04/Program.cs
@@ -1,5 +1,15 @@
 ﻿using (HttpClient client = new HttpClient())
 {
-    string reposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    Console.WriteLine(reposta);
+    
+    try
+    {
+        string reposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(reposta);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+
+    
 }
