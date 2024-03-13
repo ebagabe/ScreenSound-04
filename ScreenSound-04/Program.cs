b56e4c5b66346255a378f955458f20fbfad1b081
ebagabe <gabriel.ebagabe@gmail.com>
2024-03-13 20:31:32 -0300

    Deserializacao dos dados

--- ScreenSound-04/Program.cs
+++ ScreenSound-04/Program.cs
@@ -1,11 +1,15 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using ScreenSound_04.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
     
     try
     {
-        string reposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(reposta);
-    }
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1998].ExibirDetalhesDaMusica();
+    }   
     catch (Exception ex)
     {
         Console.WriteLine($"Temos um problema: {ex.Message}");
