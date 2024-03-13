8d6b31504d43427662eef436a95cbd1746152fb0
ebagabe <gabriel.ebagabe@gmail.com>
2024-03-13 20:48:25 -0300

    Selecionando apenas generos musicas utilizando Linq

---
+++ ScreenSound-04/Filtros/LinqFilter.cs
@@ -0,0 +1,17 @@
+﻿using ScreenSound_04.Modelos;
+
+namespace ScreenSound_04.Filtros;
+
+internal class LinqFilter
+{
+    public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+    {
+        var todosOsGenerosMusicais = musicas.Select(generos => 
+            generos.Genero).Distinct().ToList();
+
+        foreach (var genero in todosOsGenerosMusicais)
+        {
+            Console.WriteLine($"- {genero}");
+        }
+    }
+}
