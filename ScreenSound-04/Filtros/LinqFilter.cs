0abc4c117d260f8a11109c001e5e8aba7327e9a3
ebagabe <gabriel.ebagabe@gmail.com>
2024-03-13 21:12:50 -0300

    Filtrando artistas por genero musical utilizando Linq

--- ScreenSound-04/Filtros/LinqFilter.cs
+++ ScreenSound-04/Filtros/LinqFilter.cs
@@ -14,4 +14,18 @@ internal class LinqFilter
             Console.WriteLine($"- {genero}");
         }
     }
+
+    public static void FiltrarArtistaPorGeneroMusical(List<Musica> musicas, string genero)
+    {
+        var artistasPorgeneroMusical = musicas.Where(musica => musica.Genero.Contains(genero.ToLower()))
+            .Select(musica => musica.Artista)
+            .Distinct()
+            .ToList();
+
+        Console.WriteLine($"Exibindo artistas por genero musical - Genero: {genero}");
+        foreach (var artista in artistasPorgeneroMusical)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
 }
